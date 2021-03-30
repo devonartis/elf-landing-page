4619249fe1b6dccab7d136955484dd4d2165f057
Danny Delott <dannydelott@gmail.com>
2021-03-30 12:53:06 -0700

    Add image meta tags (#3)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,9 +62,9 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
           property="og:description"
           content="Element Finance offers BTC, ETH and USDC at a discount, introducing high fixed yield income to the DeFi market."
         />
-        {/* TODO: <meta property="og:image" content="" /> */}
-        <meta property="og:url" content="http://element.fi" />
-        {/* TODO: <meta name="twitter:card" content="" /> */}
+        <meta property="og:image" content="https://element.fi/element-finance.jpg" />
+        <meta property="og:url" content="https://element.fi" />
+        <meta name="twitter:card" content="summary_large_image" />
 
         {/* Non-Essential, But Recommended  */}
         <meta property="og:site_name" content="Element Finance" />
